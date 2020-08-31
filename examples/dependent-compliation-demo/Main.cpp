504327b9228340c6654c5475e439a3f434fa0df3
Jacob Heruty <jheruty@gmail.com>
2020-08-30 20:52:55 -0700

    Remove debug code from dependent-compilation-demo Main.cpp.

--- examples/dependent-compliation-demo/Main.cpp
+++ examples/dependent-compliation-demo/Main.cpp
@@ -11,7 +11,6 @@ int main()
     swapper.AddIncludeDirectory(std::filesystem::current_path());
     swapper.AddSourceDirectory(std::filesystem::current_path());
 
-    swapper.EnableFeature(hscpp::Feature::Preprocessor);
     swapper.EnableFeature(hscpp::Feature::DependentCompilation);
 
     swapper.CreateDependencyGraph();
@@ -19,21 +18,9 @@ int main()
     swapper.GetAllocationResolver()->Allocate<Printer1>();
     swapper.GetAllocationResolver()->Allocate<Printer2>();
 
-    hscpp::Callbacks callbacks;
-    callbacks.BeforeSwap = []() {std::cout << "Before swap callback." << std::endl; };
-    callbacks.AfterSwap = []() {std::cout << "After swap callback." << std::endl; };
-    callbacks.BeforePreprocessor = [](hscpp::Preprocessor::Input& input) {
-        input.sourceFilePaths.push_back(std::filesystem::current_path() / "Printer2.cpp");
-    };
-
-    swapper.SetCallbacks(callbacks);
-    swapper.EnableFeature(hscpp::Feature::ManualCompilationOnly);
-
     while (true)
     {
         swapper.Update();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        swapper.TriggerManualBuild();
     }
 }
