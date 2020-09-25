7c44a816c441b6c953b581d00fe8d4454f56e650
Jacob Heruty <jheruty@gmail.com>
2020-09-24 21:47:14 -0700

    Fix CmdShell test for Windows, where echo adds a couple newlines.

--- test/unit-tests/Test_CmdShell.cpp
+++ test/unit-tests/Test_CmdShell.cpp
@@ -7,6 +7,21 @@
 namespace hscpp { namespace test
 {
 
+    static void RemoveBlankLines(std::vector<std::string>& vec)
+    {
+        for (auto it = vec.begin(); it != vec.end();)
+        {
+            if (it->empty())
+            {
+                it = vec.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+    }
+
     TEST_CASE("CmdShell can perform a basic echo.", "[CmdShell]")
     {
         std::unique_ptr<ICmdShell> pCmdShell = platform::CreateCmdShell();
@@ -22,7 +37,9 @@ namespace hscpp { namespace test
 
             if (taskState == ICmdShell::TaskState::Done)
             {
-                const std::vector<std::string>& output = pCmdShell->PeekTaskOutput();
+                std::vector<std::string> output = pCmdShell->PeekTaskOutput();
+                RemoveBlankLines(output);
+                
                 REQUIRE(output.size() == 1);
                 REQUIRE(output.at(0) == "hello");
 
