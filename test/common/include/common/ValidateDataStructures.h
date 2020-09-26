171b110ad5c112b6088b2bb496127dcf6615abc8
Jacob Heruty <jheruty@gmail.com>
2020-09-26 00:20:23 -0700

    Add basic test for DependencyGraph.

--- test/common/include/common/ValidateDataStructures.h
+++ test/common/include/common/ValidateDataStructures.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
 #include "catch/catch.hpp"
 
 namespace hscpp { namespace test {
@@ -14,6 +18,13 @@ namespace hscpp { namespace test {
         {
             REQUIRE(expected.at(i) == result.at(i));
         }
+    }
+
+    template <typename T, typename THasher = std::hash<T>>
+    void ValidateUnorderedVector(const std::vector<T>& result, const std::vector<T>& expected)
+    {
+        REQUIRE(expected.size() == result.size());
+        REQUIRE(std::is_permutation(result.begin(), result.end(), expected.begin(), expected.end()));
     }
 
 }}
