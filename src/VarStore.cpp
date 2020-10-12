1155978fb749f8149f2b9b0da5088d6dbcd528ed
Jacob Heruty <jheruty@gmail.com>
2020-10-12 01:06:40 -0700

    Add VarStore test. Add ability to interpolate vars with spaces within the ${Var} syntax.

--- src/VarStore.cpp
+++ src/VarStore.cpp
@@ -1,17 +1,18 @@
 #include "hscpp/VarStore.h"
 #include "hscpp/Log.h"
+#include "hscpp/Util.h"
 
 namespace hscpp
 {
 
     void VarStore::SetVar(const std::string& name, const std::string& val)
     {
-        m_Vars[name] = val;
+        m_Vars[util::Trim(name)] = val;
     }
 
     bool VarStore::RemoveVar(const std::string& name)
     {
-        auto varIt = m_Vars.find(name);
+        auto varIt = m_Vars.find(util::Trim(name));
         if (varIt == m_Vars.end())
         {
             return false;
@@ -24,16 +25,33 @@ namespace hscpp
     std::string VarStore::Interpolate(const std::string& str)
     {
         std::string interpolatedStr = str;
-        for (const auto& var : m_Vars)
-        {
-            std::string search = "${" + var.first + "}";
 
-            size_t iFound = interpolatedStr.find(search);
-            while (iFound != std::string::npos)
+        // Find and replace, for example "The ${ VarName }" with "The VarValue".
+        std::string startPattern = "${";
+        std::string endPattern = "}";
+
+        size_t iStart = interpolatedStr.find(startPattern);
+        while (iStart != std::string::npos)
+        {
+            size_t iEnd = interpolatedStr.find(endPattern, iStart + 1);
+            if (iEnd != std::string::npos)
             {
-                interpolatedStr.replace(iFound, search.size(), var.second);
-                iFound = interpolatedStr.find(search);
+                size_t iMatchStart = iStart + startPattern.size();
+                size_t iMatchEnd = iEnd - endPattern.size();
+
+                std::string varName = interpolatedStr.substr(iMatchStart, iMatchEnd - iMatchStart + 1);
+                varName = util::Trim(varName);
+
+                auto varIt = m_Vars.find(varName);
+                if (varIt != m_Vars.end())
+                {
+                    interpolatedStr.replace(iStart, iEnd - iStart + 1, varIt->second);
+                }
             }
+
+            // String may have been lengthened or shortened, so continue search from the last start
+            // since that index is stable.
+            iStart = interpolatedStr.find(startPattern, iStart + 1);
         }
 
         return interpolatedStr;
---
+++ test/unit-tests/Test_VarStore.cpp
@@ -0,0 +1,63 @@
+#include "catch/catch.hpp"
+
+#include "hscpp/VarStore.h"
+
+namespace hscpp { namespace test
+{
+
+    TEST_CASE("VarStore can store and interpolate variables.")
+    {
+        VarStore store;
+
+        store.SetVar("Var1", "Var1_Value");
+        store.SetVar("TheSecond Variable", "Var2's !@$Value");
+
+        std::string str = "Our values are ${Var1} and ${   TheSecond Variable}, ${Third   } does not exist.";
+        std::string origStr = str;
+        std::string interpolatedStr = store.Interpolate(str);
+
+        REQUIRE(str == origStr);
+        REQUIRE(interpolatedStr == "Our values are Var1_Value and Var2's !@$Value, ${Third   } does not exist.");
+
+        store.RemoveVar("TheSecond Variable");
+        interpolatedStr = store.Interpolate(str);
+
+        REQUIRE(interpolatedStr == "Our values are Var1_Value and ${   TheSecond Variable}, ${Third   } does not exist.");
+
+        store.SetVar("Var2", "Var2_Value");
+        str = "Interpolate multiple times ${ Var1} and ${ Var1 } and ${Var2 } and ${   Var1   }";
+
+        interpolatedStr = store.Interpolate(str);
+
+        REQUIRE(interpolatedStr == "Interpolate multiple times Var1_Value and Var1_Value and Var2_Value and Var1_Value");
+
+        store.SetVar(" SpacesAreTrimmed ", "217");
+        str = "Padded variables are trimmed ${SpacesAreTrimmed} ${ SpacesAreTrimmed }";
+
+        interpolatedStr = store.Interpolate(str);
+
+        REQUIRE(interpolatedStr == "Padded variables are trimmed 217 217");
+    }
+
+    TEST_CASE("VarStore can handle edge cases.")
+    {
+        VarStore store;
+
+        std::string str = "{}{}{}{}{}";
+        REQUIRE(store.Interpolate(str) == "{}{}{}{}{}");
+
+        store.SetVar("Var", "Val");
+        str = "${                     Var";
+        REQUIRE(store.Interpolate(str) == "${                     Var");
+
+        str = "                 Var            }";
+        REQUIRE(store.Interpolate(str) == "                 Var            }");
+
+        str = "${                         Var                                 }";
+        REQUIRE(store.Interpolate(str) == "Val");
+
+        str = "${ ${Var}  }";
+        REQUIRE(store.Interpolate(str) == "${ Val  }");
+    }
+
+}}
