9a0dc87d81ba48d76c051bee846014b0002725ad
Jacob Heruty <jheruty@gmail.com>
2020-10-12 22:13:07 -0700

    Fix compilation error on C++11.

--- test/common/include/common/ValidateDataStructures.h
+++ test/common/include/common/ValidateDataStructures.h
@@ -20,11 +20,11 @@ namespace hscpp { namespace test {
         }
     }
 
-    template <typename T, typename THasher = std::hash<T>>
+    template <typename T>
     void ValidateUnorderedVector(const std::vector<T>& result, const std::vector<T>& expected)
     {
         REQUIRE(expected.size() == result.size());
-        REQUIRE(std::is_permutation(result.begin(), result.end(), expected.begin(), expected.end()));
+        REQUIRE(std::is_permutation(result.begin(), result.end(), expected.begin()));
     }
 
 }}
