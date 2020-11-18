fba5af372ebceb6c8827f112d934af0e59d6e2c0
Jacob Heruty <jheruty@gmail.com>
2020-11-17 21:42:42 -0800

    Improve output of ValidateOrderedVector and ValidateUnorderedVector.

--- test/common/include/common/ValidateDataStructures.h
+++ test/common/include/common/ValidateDataStructures.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sstream>
 #include <vector>
 #include <unordered_set>
 #include <algorithm>
@@ -9,8 +10,31 @@
 namespace hscpp { namespace test {
 
     template <typename T>
+    std::string ToString(const std::vector<T>& vec)
+    {
+        std::stringstream ss;
+        ss << "{\n";
+
+        for (size_t i = 0; i < vec.size(); ++i)
+        {
+            ss << "\t" << vec.at(i);
+            if (i != vec.size() - 1)
+            {
+                ss << ",";
+            }
+
+            ss << "\n";
+        }
+
+        ss << "}\n";
+        return ss.str();
+    }
+
+    template <typename T>
     void ValidateOrderedVector(const std::vector<T>& result, const std::vector<T>& expected)
     {
+        INFO("\nResult:\n" + ToString(result) + "\nExpected:\n" + ToString(expected));
+
         REQUIRE(expected.size() == result.size());
 
         size_t nElements = expected.size();
@@ -23,6 +47,8 @@ namespace hscpp { namespace test {
     template <typename T>
     void ValidateUnorderedVector(const std::vector<T>& result, const std::vector<T>& expected)
     {
+        INFO("\nResult:\n" + ToString(result) + "\nExpected:\n" + ToString(expected));
+
         REQUIRE(expected.size() == result.size());
         REQUIRE(std::is_permutation(result.begin(), result.end(), expected.begin()));
     }
