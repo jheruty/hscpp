77257994ff8ef08ded753c7b90b4a04da97b3616
Jacob Heruty <jheruty@gmail.com>
2021-02-01 23:45:47 -0800

    Add hscpp-mem library, which wraps underlying memory with a Ref type. This memory can change without the Ref needing to be updated, allowing it to be copied across runtime swaps.

---
+++ extensions/mem/include/hscpp/mem/Ref.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "hscpp/mem/IMemoryManager.h"
+
+namespace hscpp { namespace mem {
+
+    template <typename T>
+    class Ref
+    {
+        friend class MemoryManager;
+
+    public:
+        T* operator->() const
+        {
+            return GetMemory();
+        }
+
+        T* operator*() const
+        {
+            return GetMemory();
+        }
+
+        T* operator&() const
+        {
+            return GetMemory();
+        }
+
+    protected:
+        uint64_t m_Id = IMemoryManager::INVALID_ID;
+        IMemoryManager* m_pMemoryManager = nullptr;
+
+        // Throws an std::runtime_error on a nullptr access. This makes it possible to catch
+        // a null dereference on Linux and macOS with the hscpp::DoProtectedCall wrapper, and fix
+        // the error during runtime (on Windows, a nullptr exception can always be caught).
+        T* GetMemory() const
+        {
+            if (m_pMemoryManager == nullptr)
+            {
+                throw std::runtime_error("Ref is null (MemoryManager == nullptr).");
+            }
+
+            T* pT = reinterpret_cast<T*>(m_pMemoryManager->GetMemory(m_Id));
+            if (pT == nullptr)
+            {
+                throw std::runtime_error("Ref is null.");
+            }
+
+            return pT;
+        }
+
+        // No std::runtime_error throw on a nullptr access.
+        T* GetMemoryUnsafe() const
+        {
+            if (m_pMemoryManager == nullptr)
+            {
+                return nullptr;
+            }
+
+            return reinterpret_cast<T*>(m_pMemoryManager->GetMemory(m_Id));
+        }
+
+    };
+
+    template <typename T>
+    class UniqueRef : public Ref<T>
+    {
+        friend class MemoryManager;
+
+        // Do not allow copy.
+        UniqueRef(const UniqueRef<T>& rhs) = delete;
+        UniqueRef<T>& operator=(const UniqueRef<T>& rhs) = delete;
+
+    public:
+        UniqueRef() = default;
+
+        UniqueRef(UniqueRef<T>&& rhs) noexcept
+        {
+            MoveRef(std::move(rhs));
+        }
+
+        UniqueRef<T>& operator=(UniqueRef<T>&& rhs) noexcept
+        {
+            Free();
+            MoveRef(std::move(rhs));
+            return *this;
+        }
+
+        void Free()
+        {
+            if (this->m_Id == IMemoryManager::MEMORY_MANAGER_ID)
+            {
+                // This Ref refers to the MemoryManager itself. Do a normal delete, as the
+                // MemoryManager will allocate no Block for its own instance.
+                delete this->m_pMemoryManager;
+                this->m_pMemoryManager = nullptr;
+            }
+            else
+            {
+                // Free memory, unless this Ref is a nullptr (double-freeing a Ref is safe).
+                T* pSelf = this->GetMemoryUnsafe();
+                if (pSelf != nullptr)
+                {
+                    pSelf->~T();
+                    this->m_pMemoryManager->FreeBlock(this->m_Id);
+                }
+            }
+
+            this->m_Id = IMemoryManager::INVALID_ID;
+        }
+
+        ~UniqueRef()
+        {
+            Free();
+        }
+
+    private:
+        void MoveRef(UniqueRef<T>&& rhs) noexcept
+        {
+            this->m_Id = rhs.m_Id;
+            this->m_pMemoryManager = rhs.m_pMemoryManager;
+
+            rhs.m_Id = IMemoryManager::INVALID_ID;
+            rhs.m_pMemoryManager = nullptr;
+        }
+    };
+
+}}
---
+++ extensions/mem/src/MemoryManager.cpp
@@ -0,0 +1,145 @@
+#include "hscpp/mem/MemoryManager.h"
+
+namespace hscpp { namespace mem {
+
+    UniqueRef<MemoryManager> MemoryManager::Create(const Config& config /*=Config()*/)
+    {
+        MemoryManager* pMemoryManager = new MemoryManager();
+        pMemoryManager->m_pAllocationResolver = config.pAllocationResolver;
+        pMemoryManager->m_AllocateCb = config.AllocateCb;
+        pMemoryManager->m_FreeCb = config.FreeCb;
+        pMemoryManager->m_Blocks.reserve(config.reservedBlocks);
+        pMemoryManager->m_FreeBlockIndices.reserve(config.reservedBlocks);
+        pMemoryManager->m_FreeBlockIndexByBlock.reserve(config.reservedBlocks);
+
+        UniqueRef<MemoryManager> ref;
+        ref.m_pMemoryManager = pMemoryManager;
+        ref.m_Id = IMemoryManager::MEMORY_MANAGER_ID;
+
+        return std::move(ref);
+    }
+
+    uint64_t MemoryManager::GetNumBlocks() const
+    {
+        return m_iFreeBlocksBegin;
+    }
+
+    uint8_t* MemoryManager::GetMemory(uint64_t id)
+    {
+        switch (id)
+        {
+            case IMemoryManager::INVALID_ID:
+                return nullptr;
+            case IMemoryManager::MEMORY_MANAGER_ID:
+                return reinterpret_cast<uint8_t*>(this);
+            default:
+                return m_Blocks.at(id).pMemory;
+        }
+    }
+
+    void MemoryManager::FreeBlock(uint64_t iBlock)
+    {
+        switch (iBlock)
+        {
+            case IMemoryManager::INVALID_ID:
+                break; // Equivalent to deleting a nullptr.
+            case IMemoryManager::MEMORY_MANAGER_ID:
+                break; // MemoryManager instance does not use any Block.
+            default:
+                m_FreeCb(m_Blocks.at(iBlock).pMemory - sizeof(BlockHeader));
+                m_Blocks.at(iBlock).pMemory = nullptr;
+
+                // Get the index of the last Block that is not free, in the free Block list.
+                size_t iLastUsedBlock = m_FreeBlockIndices.at(m_iFreeBlocksBegin - 1);
+
+                // Map the Block index to the free Block list.
+                size_t iFreeBlock = m_FreeBlockIndexByBlock.at(iBlock);
+
+                // Swap freed Block with last taken Block before the free Blocks. Since the
+                // number of free Blocks will be decremented, this will push the freed Block
+                // into the free list.
+                std::swap(m_FreeBlockIndices.at(iFreeBlock), m_FreeBlockIndices.at(m_iFreeBlocksBegin - 1));
+
+                // Update the free Block list mapping.
+                std::swap(m_FreeBlockIndexByBlock.at(iBlock), m_FreeBlockIndexByBlock.at(iLastUsedBlock));
+
+                m_iFreeBlocksBegin--;
+                break;
+        }
+    }
+
+    AllocationInfo MemoryManager::Hscpp_Allocate(uint64_t size)
+    {
+        // Performing a generic allocation through hscpp.
+        uint64_t iBlock = ReserveFirstFreeBlock();
+        uint8_t* pMemory = AllocateBlock(size, iBlock);
+
+        AllocationInfo info;
+        info.id = iBlock;
+        info.pMemory = pMemory;
+
+        return info;
+    }
+
+    AllocationInfo MemoryManager::Hscpp_AllocateSwap(uint64_t previousId, uint64_t size)
+    {
+        // Performing a runtime swap of an HSCPP_TRACK object. Reuse the old Block, so that old
+        // Refs will now refer to the newly allocated class.
+        uint64_t iBlock = previousId;
+        AllocateBlock(size, iBlock);
+
+        AllocationInfo info;
+        info.id = iBlock;
+        info.pMemory = m_Blocks.at(iBlock).pMemory;
+
+        return info;
+    }
+
+    uint64_t MemoryManager::Hscpp_FreeSwap(uint8_t* pMemory)
+    {
+        // Performing a free during a runtime swap. Return the old object id so that
+        // HscppAllocateSwap knows the previous id of the deleted object.
+        uint64_t iBlock = reinterpret_cast<BlockHeader*>(pMemory - sizeof(BlockHeader))->iBlock;
+        FreeBlock(iBlock);
+
+        return iBlock;
+    }
+
+    uint8_t* MemoryManager::AllocateBlock(uint64_t size, uint64_t iBlock)
+    {
+        // Allocate sizeof(BlockHeader) extra space, allowing Block info to be saved alongside
+        // the pointer. This makes it possible to quickly find the index during an Hscpp_FreeSwap.
+        uint8_t* pMemory = m_AllocateCb(size + sizeof(BlockHeader));
+
+        reinterpret_cast<BlockHeader*>(pMemory)->iBlock = iBlock;
+
+        // Return memory past the BlockHeader.
+        m_Blocks.at(iBlock).pMemory = pMemory + sizeof(BlockHeader);
+        return m_Blocks.at(iBlock).pMemory;
+    }
+
+    uint64_t MemoryManager::ReserveFirstFreeBlock()
+    {
+        if (m_iFreeBlocksBegin == m_FreeBlockIndices.size())
+        {
+            // No free blocks remain, push back a new one.
+            m_FreeBlockIndices.push_back(m_iFreeBlocksBegin);
+            m_FreeBlockIndexByBlock.push_back(m_iFreeBlocksBegin);
+            m_Blocks.push_back(Block());
+        }
+
+        m_iFreeBlocksBegin++;
+        return m_FreeBlockIndices.at(m_iFreeBlocksBegin - 1);
+    }
+
+    MemoryManager::Config::Config()
+    {
+        AllocateCb = [](uint64_t size) {
+            return new uint8_t[size];
+        };
+
+        FreeCb = [](uint8_t* pMemory) {
+            delete[] pMemory;
+        };
+    }
+}}
